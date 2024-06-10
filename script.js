e1658fa6befeb70848b5b214c6f858d21bb96a09
Sayan Chatterjee <155249320+coderRemo@users.noreply.github.com>
2024-06-11 00:56:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
-// console.log(btnOpenModal);
 
 const openModal = function () {
   modal.classList.remove('hidden');
@@ -23,3 +22,10 @@ for (let i = 0; i < btnOpenModal.length; i++) {
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+// handling ESC keypress event
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
